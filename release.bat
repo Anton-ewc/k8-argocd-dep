8d74a5316ab8e302dd91f46bcc4876d1b6d6c579
A T <anton@adsen-media.com>
2026-03-16 14:23:59 +0200

    release arg $date

---
+++ release.bat
@@ -0,0 +1,9 @@
+set date=%date%
+echo %date%
+git add .
+git commit -m "release arg $date"
+git push
+git tag arg
+git push origin arg
+
+gh release create arg --title "release arg $date" --notes "release arg $date"
